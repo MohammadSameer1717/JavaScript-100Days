b79f9492f9a816c30e0ebd95c389d12c9975d877
MohammadSameer1717 <mohammadsameer0893@gmail.com>
2025-07-22 01:33:08 +0530

    map-filter-reduce

---
+++ map-filter-reduce.js
@@ -0,0 +1,96 @@
+const arr = [5, 1, 3, 2, 6];
+double = [10, 2, 6, 4, 12]
+triple = [15, 3, 9, 6, 18]
+Binary = ["101", "1", "11", "10", "110"]
+
+
+
+function  double(x) {
+    return x * 2;
+}
+
+function  triple(x) {
+    return x * 3;
+}
+
+
+ 
+
+
+const output = arr.map(function binary(x) {
+    return x.toString(2);
+});
+
+const output = arr.map((x) => x.toString (2));
+console.log(output);
+
+
+const arr = [5, 1, 3, 2, 6];
+// Filter odd values
+function isOdd(x) {
+    return x % 2 ;
+}
+const output = arr.filter(isOdd);
+console.log(output);
+
+const arr = [5, 1, 3, 2, 6];
+// Filter Even values
+function isEven(x) {
+    return x % 2 === 0;
+}
+const output = arr.filter(isEven);
+console.log(output);
+
+const arr = [5, 1, 3, 2, 6];
+ 
+const output = arr.filter((x) => x > 4);
+console.log(output);
+
+
+// // sum of maximum value
+function findSum(arr) {
+    let sum = 0;
+    for ( let i = 0; i < arr.length; i++) {
+        sum = sum + arr[i];
+    }
+    return sum;
+}
+console.log(findSum(arr));
+
+const output = arr.reduce(function (acc, curr) {
+    acc = acc + curr;
+}, 0);
+console.log(output);
+
+const users = [
+    { firstName: "akshay",lastName: "saini", age: 28 },
+    { firstName: "sameer",lastName: "peter", age: 25 },
+    { firstName: "salman",lastName: "ali khan", age: 26 },
+    { firstName: "samarth",lastName: "saxena", age: 24 },
+     { firstName: "pavan",lastName: "saxena", age: 24 },
+];
+
+const output = users.map((user) => user.firstName);
+console.log(output);
+
+// method reduce => age = 
+const output = users.reduce(function (acc, curr) {
+    if (acc[curr.age]) {
+        acc[curr.age] = ++acc[curr.age];
+    } else {
+        acc[curr.age] = 1;
+    }
+    return acc;
+}, {});
+
+console.log(output);
+
+const output = 
+
+users.filter((x) => x.age < 30)
+.map((x) => x.firstName);
+
+console.log(output);
+
+
+
