74450b658c682f155b995fb136dd439965a0cc2d
MohammadSameer1717 <mohammadsameer0893@gmail.com>
2025-07-29 00:20:07 +0530

    Revision-02

---
+++ objectmethods.js
@@ -0,0 +1,44 @@
+// var product1 = {
+//     id: 1234,
+//     name: "xyz",
+//     const: 5000,
+//     category: "Random",
+// };
+
+// var product2 = {
+//     name: "abc",
+//     createdAt: new Date()
+// };
+
+// this is the method of objectmethods.js******
+
+// Object.assign(product1, product2);
+// console.log(product1);
+
+// var check = Object.entries(product1);
+// console.log(check);
+
+
+// var check =Object.keys(product1);
+// var check1 = Object.values(product1);
+
+// console.log(check);
+// console.log(check1);
+
+
+
+ var user = {
+    id: 1234,
+    name: "abc",
+    emial: "abc@gmail.com"
+};
+// Object.freeze(user);
+Object.seal(user);
+
+// user["test"] = "nmcunfwe";
+
+// user.name = "test";
+
+// delete user.id;
+
+console.log(user);
