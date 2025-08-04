62ab1c6550b6b17fac0cc124f92c952dc9abbbd8
MohammadSameer1717 <mohammadsameer0893@gmail.com>
2025-08-05 00:25:22 +0530

    Advance js

--- jsEverythingAboutEverything/script.js
+++ jsEverythingAboutEverything/script.js
@@ -16,34 +16,52 @@
 //  abcd();
 
 // // Encapsulation 
-function clicklimiter() {
-    let click = 0;
-    return function() {
-        if (click < 5) {
-            click++;
-            console.log(`clicked : ${click} times`);
-    } else; {
-        console.error ("LIMIT EXCEEDED, TRY AFTER SOOME TIME");
-     }
-   };
-}
-let fnc = clicklimiter();
-fnc();
-fnc();
+// function clicklimiter() {
+//     let click = 0;
+//     return function() {
+//         if (click < 5) {
+//             click++;
+//             console.log(`clicked : ${click} times`);
+//     } else; {
+//         console.error ("LIMIT EXCEEDED, TRY AFTER SOOME TIME");
+//      }
+//    };
+// }
+// let fnc = clicklimiter();
+// fnc();
+// fnc();
 
 // Toaster 
 function createToaster(config) {
-    return function() {
+    return function(str) {
         let div = document.createElement("div");
+        div.textContent = str;
         div.className =
-        "fixed bg-gray-800 text-white px-6 py-3 rounded shadow-lg pointer-events-none";
+        `inline-block ${config.theme ===  "dark" ? "bg-gray-800 text-white" : "bg-gray-100 text-black"} 
+        px-6 py-3 rounded shadow-lg pointer-events-none `;
+
+        document.querySelector(".parent").appendChild(div);
+
+        if (config.positionX !== "left" || config.positionY !== "top") {
+  document.querySelector(".parent").className +=
+    ` ${config.positionX === "right" ? "right-5" : "left-5"}` +
+    ` ${config.positionY === "top" ? "top-5" : "bottom-5"}`;
+}
+
+        setTimeout(() => {
+            document.querySelector(".parent").removeChild(div);
+        }, config.duration * 1000);
     };
 }
 
-createToaster({
+let toaster= createToaster({
     positionX: "right",
     positionY: "top",
     theme: "dark",
     duration: 3,
 });
-toaster("This is a dummy notification !");
+toaster("Download Done");
+setTimeout(() => {
+    toaster("Download peter funnction Done");
+}, 3000);
+ 
