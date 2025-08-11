7b81aee0d1ae541527bca46f68ac6258fb0c77ff
MohammadSameer1717 <mohammadsameer0893@gmail.com>
2025-08-11 23:53:26 +0530

    Revision

---
+++ NamasteJavascript/Revision.js
@@ -0,0 +1,11 @@
+// Using Async/Await (Cleaner Syntax)
+ async function getPost() {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts/1");
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+getPost();
