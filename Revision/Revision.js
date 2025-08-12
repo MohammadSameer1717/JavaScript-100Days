b5687dfc012435536254f0966b1e07ed73da5d26
MohammadSameer1717 <mohammadsameer0893@gmail.com>
2025-08-13 01:59:30 +0530

    Revision-01

--- Revision/Revision.js
+++ Revision/Revision.js
@@ -50,3 +50,62 @@ api.createOrder(cart, function () {
     api.proceedToPayment(function () {
     });
 });
+
+// Real Example caused that callback Hell 
+fs.readFile("file", function(err, data1) {
+    if(err) throw err;
+    fs.readFile("file2.txt", function(err, data2) {
+        if(err) throw err;
+        fs.readFile("file", function(err, data3) {
+            if (err) throw err;
+            console.log(data1, data2, data3);
+        })
+    })
+})
+
+// Promises passing a function 
+const cart1 = ["shoes", "pant", "kurte"];
+createOrder(cart, function (orderId) {
+    proceedTopayment(orderId);
+});
+// To ataching a function this one is best
+const Promises = createOrder(cart);
+Promises.then(function (orderId) {
+    proceedTopayment(orderId)
+});
+
+// // 2nd
+const GITHUB_API = "https://github.com/";
+const user = fetch(GITHUB_API);
+console.log(user);
+
+ const cart = ["shoes", "pant", "kurte"];
+createOrder(cart, function (orderId) {
+    proceedTopayment(orderId, function (paymentInfo) {
+        showOrderSummary(paymentInfo, function () {
+            updateWalletBalance();
+        });
+    });
+});
+ 
+createOrder(cart)
+.then(function (orderId) {
+ return proceedTopayment(orderId);
+})
+.then(function (paymentInfo) {
+ return proceedTopayment(paymentInfo);
+})
+.then(function (paymentInfo) {
+ return proceedTopayment(paymentInfo);
+});
+
+// // ***///
+createOrder(cart)
+.then((orderId) => proceedTopayment(orderId))
+.then((paymentInfo) => showOrderSummary(paymentInfo))
+.then((paymentInfo) => updateWalletBalance(paymentInfo));
+
+fetchData()
+  .then(result => console.log("Success:", result))
+  .catch(error => console.log("Error:", error))
+  .finally(() => console.log("Done"));
