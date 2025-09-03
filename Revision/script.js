fbe99ff067b94ca15daff6cdaa8e7dbd1309e885
MohammadSameer1717 <mohammadsameer0893@gmail.com>
2025-09-03 23:25:55 +0530

    Promises-Complete

--- Revision/script.js
+++ Revision/script.js
@@ -1,59 +1,59 @@
-function main () {
-    var name = "peter";
-    function sayName() {
-        console.log(name);
-    }
-    return sayName;
-}
-let fn = main();
-fn();
-fn();
+// function main () {
+//     var name = "peter";
+//     function sayName() {
+//         console.log(name);
+//     }
+//     return sayName;
+// }
+// let fn = main();
+// fn();
+// fn();
 
-function main (name) {
-    function sayName() {
-        console.log(name);
-    }
-    return sayName;
-}
-let consolepeter = main("peter");
+// function main (name) {
+//     function sayName() {
+//         console.log(name);
+//     }
+//     return sayName;
+// }
+// let consolepeter = main("peter");
 
-consolepeter();
-consolepeter();
-consolepeter();
+// consolepeter();
+// consolepeter();
+// consolepeter();
 
-function adder(num) {
-    function add(b) {
-        console.log(num + b);
-    }
-    return add;
-}
-const addTo5 = adder(5);
-const addTO10 = adder(10);
-const addTO50 = adder(50);
+// function adder(num) {
+//     function add(b) {
+//         console.log(num + b);
+//     }
+//     return add;
+// }
+// const addTo5 = adder(5);
+// const addTO10 = adder(10);
+// const addTO50 = adder(50);
 
-addTo5(2);
-addTo5(5);
+// addTo5(2);
+// addTo5(5);
 
-addTO10(10)
-addTO50(50)
+// addTO10(10)
+// addTO50(50)
 
 
-const myName = document.getElementById("my-name");
-const btn = document.getElementById('my-btn')
+// const myName = document.getElementById("my-name");
+// const btn = document.getElementById('my-btn')
 
-function makeTextSizer(size) {
+// function makeTextSizer(size) {
 
-    function changeSize() {
-        myName.style.fontSize = `${size}px`;
-    }
-    return changeSize;
-}
-const size12 = makeTextSizer(12);
-const size20 = makeTextSizer(20);
-const size40 = makeTextSizer(40);
-const size70 = makeTextSizer(70);
+//     function changeSize() {
+//         myName.style.fontSize = `${size}px`;
+//     }
+//     return changeSize;
+// }
+// const size12 = makeTextSizer(12);
+// const size20 = makeTextSizer(20);
+// const size40 = makeTextSizer(40);
+// const size70 = makeTextSizer(70);
 
-btn.addEventListener("click", size40);
+// btn.addEventListener("click", size40);
 
 
 const myName = document.getElementById("my-name");
@@ -72,3 +72,34 @@ counter1();
 counter1();
 counter1();
 counter1()
+
+
+// All Q / A Promises
+
+let a = 10;
+    let b = 30;
+
+    let result = a + b;
+  console.log(result);
+  
+
+
+
+//  async function getData(){
+//          let resultFromServer = await fetch(
+//         "https://jsonplaceholder.typicode.com/todos/1"
+//         );
+//     console.log(await resultFromServer.json());
+//   }
+//   getData();
+
+
+
+//     fetch( "https://jsonplaceholder.typicode.com/todos/1")
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
+//   console.log(result);
